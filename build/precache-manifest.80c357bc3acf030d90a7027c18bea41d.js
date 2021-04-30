4c6bd32c14798043abb4820e5e2cc4c23fcdce21
serge_avila7 <serge_avila7@yahoo.com>
2021-04-30 16:11:58 -0500

    first commit

---
+++ build/precache-manifest.80c357bc3acf030d90a7027c18bea41d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "64b22d2c886bd5e8ff5a",
+    "url": "/static/css/main.e1ba9017.chunk.css"
+  },
+  {
+    "revision": "64b22d2c886bd5e8ff5a",
+    "url": "/static/js/main.64b22d2c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1f819c26a2948ee25496",
+    "url": "/static/css/2.fa10347c.chunk.css"
+  },
+  {
+    "revision": "1f819c26a2948ee25496",
+    "url": "/static/js/2.1f819c26.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "c1d204d78b0af4ce36343e22502ef4c1",
+    "url": "/index.html"
+  }
+];
